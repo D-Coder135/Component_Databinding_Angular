9245bde254dcd574187a30d58815c33dd3fef6a7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-26 22:08:03 +0530

    add the @Output decorator inorder to make the event accessible from outside that particular component within which it was created

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,8 +7,8 @@ import {Component, EventEmitter, OnInit} from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
-  serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
   newServerName = '';
   newServerContent = '';
