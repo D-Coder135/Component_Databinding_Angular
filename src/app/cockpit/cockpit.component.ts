8edc2f339814ca60d58071e37d3acab1909a537a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-28 18:24:07 +0530

    comment out some part of the code

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() {
